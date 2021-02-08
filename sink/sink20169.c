caef0253c7a3ec3859d2771a9d1e0a8e9c7b3f06
Wolfgang Hottgenroth <w.hottgenroth@krohne.com>
2021-02-08 15:41:06 +0100

    changes

--- sink/sink20169.c
+++ sink/sink20169.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+
 
 int main() {
-    printf("Hello world!\n");
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(20169);
+
+    bind(sockfd, (SA*) &servaddr, sizeof(servaddr));
+
+    uint8_t buf[1024];
+
+    while (1) {
+        int n = recv(sockfd, buf, sizeof(buf), 0);
+        printf("received %d octets\n");
+    }
+
 }
