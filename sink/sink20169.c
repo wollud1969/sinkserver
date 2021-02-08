eaee6e04c9a8843e0e175839cab931c7a435d690
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2021-02-08 16:00:36 +0100

    fix

--- sink/sink20169.c
+++ sink/sink20169.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
 
 
 int main() {
     int sockfd;
-    struct sockaddr servaddr;
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t cliaddrlen = sizeof(cliaddr);
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -15,13 +17,15 @@ int main() {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(20169);
 
-    bind(sockfd, (SA*) &servaddr, sizeof(servaddr));
+    bind(sockfd, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 
     uint8_t buf[1024];
 
     while (1) {
-        int n = recv(sockfd, buf, sizeof(buf), 0);
-        printf("received %d octets\n");
+        int n = recvfrom(sockfd, buf, sizeof(buf), 0,
+                         (struct sockaddr *) &cliaddr, &cliaddrlen);
+        printf("received %d octets from %04x\n", 
+               n, cliaddr.sin_addr.s_addr);
     }
 
 }
