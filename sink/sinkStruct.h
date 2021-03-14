1626bd7030e911296f150bb722926a781cd43417
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2021-03-14 21:22:03 +0100

    missing files

---
+++ sink/sinkStruct.h
@@ -0,0 +1,25 @@
+#ifndef _SINKSTRUCT_H_
+#define _SINKSTRUCT_H_
+
+#include <stdint.h>
+#include <sha256.h>
+#include <config.h>
+
+#define SECONDS_PER_MINUTE 60
+typedef struct __attribute__((__packed__)) {
+    char deviceId[sizeof(((t_configBlock*)0)->deviceId)];
+    uint8_t hash[SHA256_BLOCK_SIZE];
+    uint32_t totalRunningHours;
+    uint32_t totalPowercycles;
+    uint32_t totalWatchdogResets;
+    uint32_t version;
+    uint64_t timestamp;
+    uint32_t frequency[SECONDS_PER_MINUTE];
+} t_minuteStruct;
+
+typedef union {
+    t_minuteStruct s;
+    uint8_t b[sizeof(t_minuteStruct)];
+} t_minuteBuffer;
+
+#endif // _SINKSTRUCT_H_
