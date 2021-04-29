b08efc4db4da473736af45ea74a947fc8ff548ce
Wolfgang Hottgenroth <w.hottgenroth@krohne.com>
2021-04-29 17:55:29 +0200

    check

--- schema/createSchema.sql
+++ schema/createSchema.sql
@@ -3,7 +3,7 @@ CREATE SEQUENCE device_s START WITH 1 INCREMENT BY 1;
 CREATE TABLE device_t (
     id integer PRIMARY KEY DEFAULT NEXTVAL('device_s'),
     deviceid varchar(16) UNIQUE NOT NULL,
-    sharedsecret varchar(31) NOT NULL,
+    sharedsecret varchar(31) NOT NULL CHECK(char_length(sharedSecret) = 31),
     location varchar(128) NOT NULL,
     active boolean NOT NULL DEFAULT false
 );
